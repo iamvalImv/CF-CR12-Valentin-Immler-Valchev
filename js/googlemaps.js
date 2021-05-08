c8afa47e1abf3640d202af959f69cd5e0242b64b
Valentin Immler-Valchev <vvvalvalchev@gmai.com>
2021-05-08 15:40:56 +0200

    JavsScript get location

--- js/googlemaps.js
+++ js/googlemaps.js
@@ -1,15 +1,21 @@
 //load the map calling the function loadMap
 function loadMap() {
   // The location of Uluru
-  const plovdiv = { lat:47.07487, lng: 12.69525 };
+   const myLat = parseFloat(document.getElementById("myLat").value);
+   const myLong = parseFloat(document.getElementById("myLong").value);
+
+  const myLocation = {
+    lat: myLat,
+    lng: myLong
+  };
   // The map, centered at Uluru
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 10,
-    center: plovdiv,
+    center: myLocation,
   });
   // The marker, positioned at Uluru
   const marker = new google.maps.Marker({
-    position: plovdiv,
+    position: myLocation,
     map: map,
   });
 
